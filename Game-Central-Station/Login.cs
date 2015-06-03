3d6673a86c7713abc61c68e63a5fe9c3dedcde87
mgosselinUTC <mgosselin@utc4me.org>
2015-06-03 11:40:58 -0400

    boopoop?

---
+++ Game-Central-Station/Login.cs
@@ -0,0 +1,84 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GameCentralStation
+{
+    public partial class Login : Form
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void Login_Load(object sender, EventArgs e)
+        {
+            if (Globals.userName != null) Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            attemptLogin();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void textBox2_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Return)
+            {
+                attemptLogin();
+            }
+        }
+
+        public void attemptLogin()
+        {
+            MySqlDataReader reader;
+            try
+            {
+                Globals.maintainDatabaseConnection();
+                MySqlCommand command = new MySqlCommand("select * from accounts where username = \"" + textBox1.Text + "\";");
+                command.Connection = Globals.connection;
+                reader = command.ExecuteReader();
+                reader.Read();
+                int passhash = Int32.Parse(reader["password"].ToString());
+                if (Globals.hash(textBox2.Text) == passhash)
+                {
+                    Globals.userName = textBox1.Text;
+                    reader.Close();
+                    Close();
+
+                }
+                else
+                {
+                    MessageBox.Show("Username and password combination incorrect.");
+                    reader.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Username and password combination incorrect.", "Error: " + ex.Message);
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            new Signup(textBox1.Text, textBox2.Text).ShowDialog();
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
